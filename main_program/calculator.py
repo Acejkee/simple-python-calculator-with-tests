cd1c4882ef1a918241d706ef420ee19fe8dc7d89
belousow_ilya <belousow.ilja@yandex.ru>
2022-01-29 12:34:52 +0500

    Добавлен тестовый принт

--- main_program/calculator.py
+++ main_program/calculator.py
@@ -1,4 +1,5 @@
 def calculator(expression):
+    print("Проверка")
     allowed = "+-/*"
     if not any(sign in expression for sign in allowed):
         raise ValueError(f'Выражение должно содержать хотя бы один знак ({allowed})')
