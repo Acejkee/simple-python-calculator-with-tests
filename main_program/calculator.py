79f6cf44279e426783cf509241ad840d2178ebd9
belousow_ilya <belousow.ilja@yandex.ru>
2022-01-29 12:29:18 +0500

    init

---
+++ main_program/calculator.py
@@ -0,0 +1,23 @@
+def calculator(expression):
+    allowed = "+-/*"
+    if not any(sign in expression for sign in allowed):
+        raise ValueError(f'Выражение должно содержать хотя бы один знак ({allowed})')
+    for sign in allowed:
+        if sign in expression:
+            try:
+                left, rigth = expression.split(sign)
+                left, rigth = int(left), int(rigth)
+                if sign == "+":
+                    return left + rigth
+                elif sign == "-":
+                    return left - rigth
+                elif sign == "*":
+                    return left * rigth
+                elif sign == "/":
+                    return left / rigth
+            except (ValueError, TypeError):
+                raise ValueError(f'Выражение должно содержать 2 целых числа и 1 знак')
+
+
+if __name__ == '__main__':
+    print(calculator("3/2"))
---
+++ tests/calculator_test.py
@@ -0,0 +1,45 @@
+from unittest import TestCase, main
+from main_program.calculator import calculator
+
+
+class CalculatorTest(TestCase):
+    def test_plus(self):
+        self.assertEqual(calculator("2+2"), 4)
+
+    def test_minus(self):
+        self.assertEqual(calculator("3-2"), 1)
+
+    def test_multi(self):
+        self.assertEqual(calculator("4*2"), 8)
+
+    def test_devide(self):
+        self.assertEqual(calculator("10/2"), 5)
+
+    def test_no_sign(self):
+        with self.assertRaises(ValueError) as e:
+            calculator("qwerty")
+        self.assertEqual('Выражение должно содержать хотя бы один знак (+-/*)', e.exception.args[0])
+
+    def test_two_sign(self):
+        with self.assertRaises(ValueError) as e:
+            calculator("3+3+3")
+        self.assertEqual('Выражение должно содержать 2 целых числа и 1 знак', e.exception.args[0])
+
+    def test_many_sign(self):
+        with self.assertRaises(ValueError) as e:
+            calculator("3+3+3")
+        self.assertEqual('Выражение должно содержать 2 целых числа и 1 знак', e.exception.args[0])
+
+    def test_no_ints(self):
+        with self.assertRaises(ValueError) as e:
+            calculator("2.2 + 3")
+        self.assertEqual('Выражение должно содержать 2 целых числа и 1 знак', e.exception.args[0])
+
+    def test_strings(self):
+        with self.assertRaises(ValueError) as e:
+            calculator("a+b")
+        self.assertEqual('Выражение должно содержать 2 целых числа и 1 знак', e.exception.args[0])
+
+
+if __name__ == '__main__':
+    main()
