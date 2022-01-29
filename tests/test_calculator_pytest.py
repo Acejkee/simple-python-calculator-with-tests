e1306b5354a64c674ae8a0ffb53def9ac7f8715c
belousow_ilya <belousow.ilja@yandex.ru>
2022-01-29 14:15:15 +0500

    Добавлены еще 2 теста test_no_sign и test_two_sign

--- tests/test_calculator_pytest.py
+++ tests/test_calculator_pytest.py
@@ -7,5 +7,17 @@ def test_plus():
     assert calculator('2+2') == 4
 
 
+def test_no_sign():
+    with pytest.raises(ValueError) as error:
+        calculator("qwerty")
+    assert 'Выражение должно содержать хотя бы один знак (+-/*)' == error.value.args[0]
+
+
+def test_two_sign():
+    with pytest.raises(ValueError) as error:
+        calculator("3+3+3")
+    assert 'Выражение должно содержать 2 целых числа и 1 знак' == error.value.args[0]
+
+
 if __name__ == '__main__':
     pytest.main()
